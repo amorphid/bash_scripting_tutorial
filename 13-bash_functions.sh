982bbaa3004b8e33695652740a1a7b3c282acc53
Michael Pope <mpope.cr@gmail.com>
2016-03-03 14:32:05 -0800

    13

---
+++ 13-bash_functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function function_B () {
+  echo Function B.
+}
+
+function function_A () {
+  echo $1
+}
+
+function function_D () {
+  echo Function D.
+}
+
+function function_C () {
+  echo $1
+}
+
+# Pass parameter to function A
+function_A "Function A."
+function_B
+
+# Pass parameter to function C
+function_C "Function C."
+function_D
