c1533f8312f095307e36e5c5cdf1bc83300aabeb
Michael Pope <mpope.cr@gmail.com>
2016-03-03 15:56:34 -0800

    17.1

---
+++ 17.1-addition.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+let RESULT=$1+$2
+
+echo $1+$2=$RESULT ' -> # let RESULT=$1+$2'
+declare -i RESULT2
+RESULT2=$1+$2
+echo $1+$2=$RESULT2 ' -> # declare -i RESULT2; RESULT2=$1+$2'
+echo $1+$2=$(($1 + $2)) ' -> # $(($1 + $2))'
